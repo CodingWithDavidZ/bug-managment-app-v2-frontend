08499da9f58d54f1779b1f497f85d02554f1add7
David Zombar <davidzombar@gmail.com>
2022-05-05 11:30:49 -0600

    cleaned up unused code in SortBy.js

--- src/Pages/Home/SortBy.js
+++ src/Pages/Home/SortBy.js
@@ -1,24 +1,21 @@
-import React, { useContext, useId, useEffect } from 'react';
-
-import  AppContext  from '../../Context/AppContext';
+import React, { useContext, useId } from 'react';
+import AppContext from '../../Context/AppContext';
 
 function SortBy() {
-	const {sortBy, setSortBy} = useContext(AppContext);
+	const { sortBy, setSortBy } = useContext(AppContext);
 
 	function handleSortChange(e) {
-		 setSortBy({...sortBy, sortDirection: e.target.value});
-		 console.log(()=>{return sortBy})
+		setSortBy({ ...sortBy, sortDirection: e.target.value });
+		console.log(() => {
+			return sortBy;
+		});
 	}
-
-
-
 
 	return (
 		<div className='py-3 px-4 flex items-center text-sm font-medium leading-none text-gray-600 bg-gray-200 hover:bg-gray-300 cursor-pointer rounded'>
 			<p>Sort By:</p>
 			<select
 				value={sortBy.sortDirection}
-				// aria-label='select'
 				className='focus:text-indigo-600 focus:outline-none bg-transparent ml-1'
 				onChange={handleSortChange}
 			>
