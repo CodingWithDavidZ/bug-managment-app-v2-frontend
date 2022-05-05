f3f929c5971d6414a1f133353e0934a0949391dc
David Zombar <davidzombar@gmail.com>
2022-05-05 11:43:23 -0600

    Removed unused utilities

--- src/Utilities/routes.js
+++
@@ -1,8 +0,0 @@
-import Home from '../Pages/Home/Home';
-import ViewBug from '../Pages/ViewBug/ViewBug';
-
-export default [
-	{ path: '/', name: 'Home', Component: Home },
-	{ path: '/viewBug', name: 'ViewBug', Component: ViewBug },
-];
-
