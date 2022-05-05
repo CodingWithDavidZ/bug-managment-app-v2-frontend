9dd250d8253d2ab476b49efbc5c38c552f0a8b88
David Zombar <davidzombar@gmail.com>
2022-05-05 11:27:19 -0600

    cleaded up a lot of unused code in TableFramework.js and tested functionallity - all ok

--- src/Pages/Home/Table/TableFramework.js
+++ src/Pages/Home/Table/TableFramework.js
@@ -1,14 +1,11 @@
-import React, { useId, useContext, useEffect } from 'react';
+import React, {useContext } from 'react';
 import TableElement from './TableElement';
 import AppContext from '../../../Context/AppContext';
-import SortBy from '../SortBy';
 import { useQuery } from 'react-query';
 
 function TableFramework() {
-	//responsible for holding state and rendering table
-	const { setBugs, sortBy } = useContext(AppContext);
-
 	
+	const { sortBy } = useContext(AppContext);	
 	const bugs = useQuery(['allBugs', sortBy], () =>
 		fetch(`http://localhost:3000/bugs/sortOrder`, {
 			method: 'POST',
@@ -26,41 +23,17 @@ function TableFramework() {
 		})
 	);
 
-	
-	// function getAllUsers() {
-	// 	if (allUsers === [] || allUsers === undefined) {
-	// 	fetch(`http://localhost:3000/users`, {
-	// 		method: 'GET',
-	// 		credentials: 'include',
-	// 		headers: {
-	// 			'Content-Type': 'application/json'},
-	// 			}).then((r) => {
-	// 				if (r.ok) {
-	// 					r.json().then((users) => {
-	// 				setAllUsers(users);
-	// 			});
-	// 		}
-	// 	});
-	// }
-	// }
-
-	// getAllUsers();
-
-
 
 	if(bugs.isLoading) {
 		return <div>Loading...</div>
 	}
 				
 
-
-	const renderBugs = () => {
-		
+	const renderBugs = () => {		
 		return bugs.data.map((bug) => {
 			return <TableElement key={bug.id} bug={bug} bugs={bugs.data}/>;
 		});
 	};
-
 
 
 	return (
